89acb2390dc64420b8e9eaebbe0422d1d3a2b2d3
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2025-07-25 16:48:16 -0400

    Create sort_int_array.asm

---
+++ MIPS/sort_int_array.asm
@@ -0,0 +1,178 @@
+.data
+start_msg:   .asciiz "Starting Array:    "
+lth_msg:     .asciiz "Low-to-High Array: "
+htl_msg:     .asciiz "High-to-Low Array: "
+space:       .asciiz " "
+comma_space: .asciiz ", "
+newline:     .asciiz "\n"
+array:       .word 19, 4, 27, 11, 8
+array_size:  .word 5
+
+.text
+.globl main
+
+# === main ===
+main:
+    # ##########
+    # show starting array:
+    la   $a0, start_msg  # prefix
+    la   $a1, array      # array pointer
+    lw   $a2, array_size # array size
+    jal print_array
+    
+
+    # ##########
+    # order a given array from lowest to highest
+    la   $a0, array      # array pointer
+    lw   $a1, array_size # array size
+    li   $a2, 0          # 0 = increasing order
+    jal order
+    
+    # ##########
+    # print out result:
+    la   $a0, lth_msg    # prefix
+    la   $a1, array      # array pointer
+    lw   $a2, array_size # array size
+    jal print_array
+    
+    # ##########
+    # order a given array from highest to lowest
+    la   $a0, array      # array pointer
+    lw   $a1, array_size # array size
+    li   $a2, 1          # 1 = decreasing order
+    jal order
+    
+    # ##########
+    # print out result:
+    la   $a0, htl_msg    # prefix
+    la   $a1, array      # array pointer
+    lw   $a2, array_size # array size
+    jal print_array
+    
+    # ##########
+    # Exit
+    li $v0, 10
+    syscall
+    
+
+# === order ===
+# Sorts an array of integers in-place
+# Input:
+#   $a0 = base address of array
+#   $a1 = size of array (n)
+#   $a2 = direction flag (0 = ascending, 1 = descending)
+# Output:
+#   array is modified in-place
+order:
+    # Save $ra
+    addi $sp, $sp, -4
+    sw   $ra, 0($sp)
+
+    move $t0, $a0       # base address
+    move $t5, $a2       # direction flag (0 or 1)
+
+    addi $t8, $a1, -1   # outer loop upper bound: n - 1
+    li   $t1, 0         # i = 0
+
+outer_loop:
+    bge  $t1, $t8, done
+
+    li   $t2, 0         # j = 0
+
+inner_loop:
+    sub  $t9, $a1, $t1
+    addi $t9, $t9, -1   # n - i - 1
+    bge  $t2, $t9, end_inner
+
+    # Load current and next elements
+    mul  $t6, $t2, 4
+    add  $t6, $t6, $t0
+    lw   $t3, 0($t6)        # arr[j]
+    lw   $t4, 4($t6)        # arr[j+1]
+
+    # Determine if we should swap based on direction
+    beq  $t5, 0, check_increasing
+    # Direction == 1 -> decreasing
+    blt  $t3, $t4, do_swap
+    j    skip_swap
+
+check_increasing:
+    bgt  $t3, $t4, do_swap
+    j    skip_swap
+
+do_swap:
+    # Swap arr[j] and arr[j+1]
+    sw   $t4, 0($t6)
+    sw   $t3, 4($t6)
+
+skip_swap:
+    addi $t2, $t2, 1
+    j    inner_loop
+
+end_inner:
+    addi $t1, $t1, 1
+    j    outer_loop
+
+done:
+    # Restore $ra and return
+    lw   $ra, 0($sp)
+    addi $sp, $sp, 4
+    jr   $ra
+    
+
+
+# === print_array ===
+# Prints a prefix followed by an array of integers separated by commas
+# Input:
+#   $a0 = prefix string
+#   $a1 = array pointer
+#   $a2 = array size (n)
+print_array:
+    # Prologue
+    addi $sp, $sp, -8
+    sw   $ra, 4($sp)
+    sw   $s0, 0($sp)
+
+    # Print prefix
+    li   $v0, 4
+    syscall
+
+    li   $t0, 0          # index i = 0
+    move $s0, $a0        # save array pointer (base) into $s0
+
+print_loop:
+    bge  $t0, $a1, end_print
+
+    # Load value at arr[i]
+    mul  $t1, $t0, 4     # offset = i * 4
+    add  $t1, $s0, $t1   # address = base + offset
+    lw   $a0, 0($t1)
+    li   $v0, 1          # print_int
+    syscall
+
+    # Print comma and space if not the last element
+    addi $t2, $t0, 1
+    blt  $t2, $a1, print_comma
+
+    j no_comma
+
+print_comma:
+    la   $a0, comma_space
+    li   $v0, 4
+    syscall
+
+no_comma:
+    addi $t0, $t0, 1
+    j    print_loop
+
+end_print:
+    # Print newline
+    la   $a0, newline
+    li   $v0, 4
+    syscall
+
+    # Epilogue
+    lw   $ra, 4($sp)
+    lw   $s0, 0($sp)
+    addi $sp, $sp, 8
+    jr   $ra
