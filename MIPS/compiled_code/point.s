d64cfb621adb1fba2fe54b24740ee2cecd9bd432
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2025-07-31 13:01:50 -0400

    Create point.s

---
+++ MIPS/compiled_code/point.s
@@ -0,0 +1,198 @@
+	.file	1 "point.c"
+	.section .mdebug.abi32
+	.previous
+	.nan	legacy
+	.module	fp=32
+	.module	oddspreg
+	.abicalls
+	.text
+	.align	2
+	.globl	main
+	.set	nomips16
+	.set	nomicromips
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$fp,40,$31		# vars= 8, regs= 2/0, args= 16, gp= 8
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-40
+	sw	$31,36($sp)
+	sw	$fp,32($sp)
+	move	$fp,$sp
+	move	$5,$0
+	move	$4,$0
+	.option	pic0
+	jal	new_point
+	nop
+
+	.option	pic2
+	sw	$2,28($fp)
+	lw	$4,28($fp)
+	.option	pic0
+	jal	display_point
+	nop
+
+	.option	pic2
+	lw	$4,28($fp)
+	.option	pic0
+	jal	free_point
+	nop
+
+	.option	pic2
+	move	$2,$0
+	move	$sp,$fp
+	lw	$31,36($sp)
+	lw	$fp,32($sp)
+	addiu	$sp,$sp,40
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.size	main, .-main
+	.align	2
+	.globl	new_point
+	.set	nomips16
+	.set	nomicromips
+	.ent	new_point
+	.type	new_point, @function
+new_point:
+	.frame	$fp,40,$31		# vars= 8, regs= 2/0, args= 16, gp= 8
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-40
+	sw	$31,36($sp)
+	sw	$fp,32($sp)
+	move	$fp,$sp
+	lui	$28,%hi(__gnu_local_gp)
+	addiu	$28,$28,%lo(__gnu_local_gp)
+	.cprestore	16
+	sw	$4,40($fp)
+	sw	$5,44($fp)
+	li	$4,8			# 0x8
+	lw	$2,%call16(malloc)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,malloc
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	sw	$2,28($fp)
+	lw	$2,28($fp)
+	lw	$3,40($fp)
+	sw	$3,0($2)
+	lw	$2,28($fp)
+	lw	$3,44($fp)
+	sw	$3,4($2)
+	lw	$2,28($fp)
+	move	$sp,$fp
+	lw	$31,36($sp)
+	lw	$fp,32($sp)
+	addiu	$sp,$sp,40
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	new_point
+	.size	new_point, .-new_point
+	.rdata
+	.align	2
+$LC0:
+	.ascii	"Point(%d, %d)\012\000"
+	.text
+	.align	2
+	.globl	display_point
+	.set	nomips16
+	.set	nomicromips
+	.ent	display_point
+	.type	display_point, @function
+display_point:
+	.frame	$fp,32,$31		# vars= 0, regs= 2/0, args= 16, gp= 8
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-32
+	sw	$31,28($sp)
+	sw	$fp,24($sp)
+	move	$fp,$sp
+	lui	$28,%hi(__gnu_local_gp)
+	addiu	$28,$28,%lo(__gnu_local_gp)
+	.cprestore	16
+	sw	$4,32($fp)
+	lw	$2,32($fp)
+	lw	$3,0($2)
+	lw	$2,32($fp)
+	lw	$2,4($2)
+	move	$6,$2
+	move	$5,$3
+	lui	$2,%hi($LC0)
+	addiu	$4,$2,%lo($LC0)
+	lw	$2,%call16(printf)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,printf
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	nop
+	move	$sp,$fp
+	lw	$31,28($sp)
+	lw	$fp,24($sp)
+	addiu	$sp,$sp,32
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	display_point
+	.size	display_point, .-display_point
+	.align	2
+	.globl	free_point
+	.set	nomips16
+	.set	nomicromips
+	.ent	free_point
+	.type	free_point, @function
+free_point:
+	.frame	$fp,32,$31		# vars= 0, regs= 2/0, args= 16, gp= 8
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-32
+	sw	$31,28($sp)
+	sw	$fp,24($sp)
+	move	$fp,$sp
+	lui	$28,%hi(__gnu_local_gp)
+	addiu	$28,$28,%lo(__gnu_local_gp)
+	.cprestore	16
+	sw	$4,32($fp)
+	lw	$4,32($fp)
+	lw	$2,%call16(free)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,free
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	nop
+	move	$sp,$fp
+	lw	$31,28($sp)
+	lw	$fp,24($sp)
+	addiu	$sp,$sp,32
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	free_point
+	.size	free_point, .-free_point
+	.ident	"GCC: (Ubuntu 10.3.0-1ubuntu1) 10.3.0"
+	.section	.note.GNU-stack,"",@progbits
